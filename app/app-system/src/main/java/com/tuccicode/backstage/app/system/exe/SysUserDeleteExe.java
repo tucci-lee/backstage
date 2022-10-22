f368083d4fe5cc89e090d8f990e08c18433a36fc
tucci <tucci.lee@aliyun.com>
2022-10-22 15:47:03 +0800

    修正：无法删除管理员

--- app/app-system/src/main/java/com/tuccicode/backstage/app/system/exe/SysUserDeleteExe.java
+++ app/app-system/src/main/java/com/tuccicode/backstage/app/system/exe/SysUserDeleteExe.java
@@ -1,7 +1,10 @@
 package com.tuccicode.backstage.app.system.exe;
 
+import com.tuccicode.backstage.domain.core.exception.BackstageBizCode;
+import com.tuccicode.backstage.domain.system.entity.SysUser;
 import com.tuccicode.backstage.domain.system.service.SysUserService;
 import com.tuccicode.concise.dto.Response;
+import com.tuccicode.concise.exception.Assert;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,6 +20,9 @@ public class SysUserDeleteExe {
     }
 
     public Response execute(Long uid) {
+        // 不可以删除管理员
+        Assert.isTrue(!SysUser.isAdmin(uid), BackstageBizCode.PARAMETER_ERROR);
+
         sysUserService.delete(uid);
         return Response.success();
     }
